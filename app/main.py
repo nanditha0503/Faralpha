0f0458f8038a671d58dd4c8e7670a91950e33e20
nanditha0503 <nanditha.scs24@bmsce.ac.in>
2025-07-11 18:40:37 +0530

    pull branches

--- app/main.py
+++ app/main.py
@@ -12,5 +12,7 @@ async def submit(data: dict):
     return {"status": "received", "data": data}
 
 
-
+@app.put("/updateItem/{item_id}")
+async def update(item_id: int, item: dict):
+    return {"status": "updated", "id": item_id, "data": item}
 
